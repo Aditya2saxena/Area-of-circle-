037e26e36d8d7119b6f3c344cac299f712312acc
Aditya2saxena <152675865+Aditya2saxena@users.noreply.github.com>
2023-12-09 19:03:50 +0530

    Add files via upload

---
+++ pattern code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 4;
+    int columns = 5;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
